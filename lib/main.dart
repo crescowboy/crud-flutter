88071e08bd37e83fa4f239a796cd2bc2ba7a89a5
wcrespo <wellingtoncrespo2001@gmail.com>
2023-06-08 21:23:19 -0500

    pagina home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:crud/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:crud/services/firebase_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,46 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
-      home: Home(),
+      initialRoute: '/',
+      routes: {
+        '/': ((context) => Home()),
+      },
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: FutureBuilder<List>(
-        future: getPeople(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                return Text(snapshot.data?[index]['name']);
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            ); 
-          }
-        },
-      ),
-    );
-  }
-}
---
+++ lib/pages/home.dart
@@ -0,0 +1,39 @@
+import 'package:crud/services/firebase_service.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: FutureBuilder<List>(
+        future: getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data?[index]['name']);
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            ); 
+          }
+        },
+      ),
+    );
+  }
+}
